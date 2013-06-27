dec625db86830652ae7f6e8d5ee4a15e79e15f83
Mike Prentice <mprentice@gmail.com>
2013-06-26 20:02:14 -0400

    Add version and author to package __init__.py

--- gupta/__init__.py
+++ gupta/__init__.py
@@ -1,1 +1,11 @@
-# Placeholder to make Python module imports work
+#!/usr/bin/env python
+
+"""Gupta Event API: Create and query application event data"""
+
+__version__ = "0.5"
+__author__ = "Mike Prentice <mprentice@gmail.com>"
+
+import config
+import event
+import server
+import util
--- gupta/test/__init__.py
+++ gupta/test/__init__.py
@@ -1,1 +1,6 @@
-# Placeholder to make Python module imports work
+#!/usr/bin/env python
+
+"""Gupta Event API tests"""
+
+__version__ = "0.5"
+__author__ = "Mike Prentice <mprentice@gmail.com>"
